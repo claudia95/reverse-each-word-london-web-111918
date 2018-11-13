52add19b501c94d168a7ac9837cae88e62f163d9
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 08:47:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word(string)
- string = string.split('')
+ string = string.split("")
  reversed_string = []
  
  string.each do |char|
   reversed_string.unshift(char)
  end
- 
- return reversed_string.join('')
+ puts reversed_string.join("")
 end
